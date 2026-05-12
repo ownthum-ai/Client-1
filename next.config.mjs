3954ed210135e6bf88b5affd6863f243451eb74f
ownthum-ai <ownthumai@gmail.com>
2026-05-13 00:03:37 +0530

    chore: setup github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Client-1',
   webpack: (config, { isServer }) => {
     if (isServer && config.output) {
       config.output.chunkFilename = 'chunks/[name].js';
